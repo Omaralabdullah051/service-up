ad8a5341eb8c5b312519798c89ca4bbb7c0a615b
omaralabdullah051 <omaralabdullah051@gmail.com>
2022-04-17 13:51:43 +0400

    register component added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Register />} />
       </Routes>
     </div>
   );
--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './Login.css';
 
 const Register = () => {
-    return (
-        <div>
 
+
+    return (
+        <div className='auth-container'>
+            <h4 className='text-center font-bold mt-4 text-slate-700'>Please Register</h4>
+            <form className='form-container mt-12'>
+                <input type="text" name="name" id="name" placeholder='Your name' />
+                <input type="email" name="email" id="email" placeholder='Your Email' />
+                <input type="password" name="password" id="password" placeholder='Password' />
+                <input type="password" name="confirm-password" id="confirm-password" placeholder='Confirm password' />
+                <input className='bg-slate-700 text-white cursor-pointer' type="submit" value="Register" />
+            </form>
+            <div className='flex justify-centers items-center pl-[55px]'>
+                <div className='w-44 h-1 bg-slate-700'></div>
+                <div className='mx-2'>Or</div>
+                <div className='w-44 h-1 bg-slate-700'></div>
+            </div>
+            <button className='p-2 bg-slate-700 text-white w-[400px] block mx-auto mt-4 mb-8 font-semibold'>Google Sign In</button>
         </div>
     );
 };
